0bde54e9d3c016e2c169de0d1f8f73db79bde48b
Desmond Coacher <dcoacher@outlook.com>
2025-09-26 10:34:46 +0300

    Commit

--- app/get_users.py
+++ app/get_users.py
@@ -1,1 +1,12 @@
-#comment
+import requests
+
+# Users List Dummy Data Origin: "https://dummyjson.com/users?limit=85"
+
+download_users = requests.get("https://dummyjson.com/users?limit=85")   # Downloading Data
+
+# print(download_users)
+users = download_users.json().get("users",[])   # Printing Data
+print(users[0]["firstName"])
+
+for user in users:
+    print(user["firstName"])
